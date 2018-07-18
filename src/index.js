abbbbeff3c97b9a8e0af9a78fd5fb04ace82bedd
Catherine Lin <catherine.lin.93@gmail.com>
2018-07-18 00:40:19 -0700

    first commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
-import App from './components/App';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
