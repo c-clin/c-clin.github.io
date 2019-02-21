dd993dbb06d3b85aa840cd6c0ebaa5a5f5565f0d
Catherine Lin <catherine.lin.93@gmail.com>
2019-02-20 16:50:07 -0800

    added testing for projects

---
+++ src/components/__test__/Projects.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Projects from '../Projects';
+
+describe('Projects', () => {
+  let props = {
+    history: {
+      goBack: jest.fn()
+    }
+  };
+
+  const projects = shallow(<Projects {...props} />);
+
+  it('renders properly', () => {
+    expect(projects).toMatchSnapshot();
+  });
+
+  it('contains two headers with the correct texts', () => {
+    expect(projects.find('h2')).toHaveLength(2);
+
+    expect(
+      projects
+        .find('h2')
+        .first()
+        .text()
+    ).toEqual('Freelance Work');
+
+    expect(
+      projects
+        .find('h2')
+        .last()
+        .text()
+    ).toEqual('Personal Projects');
+  });
+
+  it('contains a go back button', () => {
+    expect(projects.find('.go-back-btn').exists()).toBe(true);
+  });
+
+  //   it('renders 9 projects', () => {
+  //       expect(projects.find(''))
+  //   })
+});
